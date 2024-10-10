a0c42125e1a1a0804f7cdfc3aec6d6351205043f
Danilo Matos <dann17dev@gmail.com>
2024-10-10 11:40:53 -0300

    Update sistema_bancario_v1_update.py
    Neste commit, adicionamos duas novas ações ao menu:
    
    Uma delas exibe o valor do limite de saque disponível por transação, e a outra informa o número de saques restantes disponíveis.

--- sistema_bancario_v1.py
+++ sistema_bancario_v1.py
@@ -2,7 +2,7 @@
 
 #Fomos contratados por um grande banco para desenvolver o seu novo sistema. Esse banco deseja 
 #modernizar suas operações e para isso escolheu a linguagem "Python"
-#Devemos implementar 03 operações (Depósito, Saque e Estrato).
+#Devemos implementar 03 operações (Depósito, Saque e Extrato).
 
 #Operação de deposito 
 
@@ -15,6 +15,8 @@ menu = """
 [1] Depositar
 [2] Sacar
 [3] Extrato
+[4] Limite de Saque por Transação
+[5] Limite de Saques Diários Restantes
 [0] Sair
 
 => """
@@ -24,6 +26,10 @@ limite = 500
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
+
+# Mensagem de boas-vindas
+nome = "Fulano"
+print(f"Bem-vindo ao banco XPTO, {nome}!")
 
 while True:
 
@@ -35,35 +41,28 @@ while True:
         if valor > 0:
             saldo += valor
             extrato += f"Depósito: R$ {valor:.2f}\n"
-
         else:
             print("Operação falhou! O valor informado do depósito é inválido.")
 
     elif opcao == "2":
-        valor = float(input("Informe qual será o valor do saque: "))
-
-        excedeu_saldo = valor > saldo
-
-        excedeu_limite = valor > limite
-
-        excedeu_saques = numero_saques >= LIMITE_SAQUES
-
-        if excedeu_saldo:
-            print("Operação falhou! Você não possui saldo suficiente.")
-
-        elif excedeu_limite:
-            print("Operação falhou! O valor do saque excede o limite permitido por transação.")
-
-        elif excedeu_saques:
-            print("Operação falhou! Número máximo de saques permitido exedido, tente nomavamente amanhã.")
-
-        elif valor > 0:
-            saldo -= valor
-            extrato += f"Saque: R$ {valor:.2f}\n"
-            numero_saques += 1
-
+        if numero_saques >= LIMITE_SAQUES:
+            print("Você não possui saques disponíveis para hoje. Tente novamente amanhã.")
         else:
-            print("Operação falhou! O valor informado é inválido.")
+            valor = float(input("Informe qual será o valor do saque: "))
+
+            excedeu_saldo = valor > saldo
+            excedeu_limite = valor > limite
+
+            if excedeu_saldo:
+                print("Operação falhou! Você não possui saldo suficiente.")
+            elif excedeu_limite:
+                print("Operação falhou! O valor do saque excede o limite permitido por transação.")
+            elif valor > 0:
+                saldo -= valor
+                extrato += f"Saque: R$ {valor:.2f}\n"
+                numero_saques += 1
+            else:
+                print("Operação falhou! O valor informado é inválido.")
 
     elif opcao == "3":
         print("\n================ EXTRATO ================")
@@ -71,15 +70,18 @@ while True:
         print(f"\nSaldo: R$ {saldo:.2f}")
         print("==========================================")
 
-        
-      
+    elif opcao == "4":
+        print(f"O limite de saque por transação é R$ {limite:.2f}.")
+
+    elif opcao == "5":
+        saques_restantes = LIMITE_SAQUES - numero_saques
+        print(f"Limite de saques diários restantes: {saques_restantes}.")
+
     elif opcao == "0":
         break
 
     else:
         print("Operação inválida, por favor selecione novamente a operação desejada.")
-
-nome = "Fulano"
 
 mensagem = f"""
 ############## Agradecimento ##############
