68ce7ccc17969b66d1aece8b0e930cc4d1646a62
Danilo Matos <dann17dev@gmail.com>
2024-10-10 11:04:27 -0300

    sistema_bancario_v1
    Sistema de comando simples como sacar, depositar, extrato e sair

---
+++ sistema_bancario_v1.py
@@ -0,0 +1,92 @@
+#desafio 
+
+#Fomos contratados por um grande banco para desenvolver o seu novo sistema. Esse banco deseja 
+#modernizar suas operações e para isso escolheu a linguagem "Python"
+#Devemos implementar 03 operações (Depósito, Saque e Estrato).
+
+#Operação de deposito 
+
+#Operação Saque
+
+#Operação Extrato 
+
+menu = """
+
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[0] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "1":
+        valor = float(input("Informe qual será o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou! O valor informado do depósito é inválido.")
+
+    elif opcao == "2":
+        valor = float(input("Informe qual será o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não possui saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite permitido por transação.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques permitido exedido, tente nomavamente amanhã.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "3":
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+
+        
+      
+    elif opcao == "0":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
+
+nome = "Fulano"
+
+mensagem = f"""
+############## Agradecimento ##############
+Olá {nome}, Espero que volte logo!
+É um prazer saber que faz parte 
+do banco XPTO, agradecemos a confiança!
+=========================================
+"""
+
+print(mensagem)
